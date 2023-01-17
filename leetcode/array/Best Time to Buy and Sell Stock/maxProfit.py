6bde70ef805a5c440d347eb01518e3edf166320b
azfarjef <muhammadazfarjef@gmail.com>
2023-01-17 08:25:33 +0800

    Best time to buy and sell stock

---
+++ leetcode/array/Best Time to Buy and Sell Stock/maxProfit.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+	def maxProfit(self, prices: List[int]) -> int:
+		min_price = float('inf')
+		max_profit = 0
+		for i in range(len(prices)):
+			if prices[i] < min_price:
+				min_price = prices[i]
+			elif prices[i] - min_price > max_profit:
+				max_profit = prices[i] - min_price
+		return max_profit
+
+def main():
+	a = Solution()
+	prices = [7,1,5,3,6,4]
+
+	result = a.maxProfit(prices)
+	print(result)
+
+if __name__ == "__main__":
+	main()
--- leetcode/array/Two Sum/twoSum.py
+++ leetcode/array/Two Sum/twoSum.py
@@ -14,13 +14,13 @@ class mySolution:
 class goodSolution:
 	def twoSum(self, nums: List[int], target: int) -> List[int]:
 		hashmap = {}
-		for i in range(len(nums)):
-			hashmap[nums[i]] = i;
-		for i in range(len(nums)):
+		length = len(nums)
+		for i in range(length):
+			hashmap[nums[i]] = i
+		for i in range(length):
 			complement = target - nums[i]
 			if (complement in hashmap and hashmap[complement] != i):
 				return ([i, hashmap[complement]])
-
 
 def main():
 	# a = mySolution()
