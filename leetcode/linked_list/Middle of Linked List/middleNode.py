e4117e105aca2d8956acc5a09f477c4cbb15978f
azfarjef <muhammadazfarjef@gmail.com>
2023-01-21 09:47:08 +0800

    Middle of Linked List

---
+++ leetcode/linked_list/Middle of Linked List/middleNode.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+class ListNode:
+	def __init__(self, val=0, next=None):
+		self.val = val
+		self.next = next
+
+	def __str__(self):
+		output = ""
+		current = self
+		while current:
+			output += str(current.val) + " -> "
+			current = current.next
+		return output
+
+class Solution:
+	def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+		slow = head
+		fast = head
+
+		while fast and fast.next:
+			slow = slow.next
+			fast = fast.next.next
+
+		return slow
+
+def main():
+	head = ListNode(1)
+	head.next = ListNode(2)
+	head.next.next = ListNode(3)
+	head.next.next.next = ListNode(4)
+
+	a = Solution()
+
+	result = a.middleNode(head)
+	print(result)
+
+if __name__ == "__main__":
+	main()
