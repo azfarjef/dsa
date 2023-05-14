916e6864fdf5f6b4ac9f06f8f261830eae814eba
azfarjef <muhammadazfarjef@gmail.com>
2023-05-15 07:17:03 +0800

    Diameter of Binary Tree

---
+++ leetcode/trees/Diameter of Binary Tree/diameterOfBinaryTree.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Time: O(n), Space: O(1)
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+
+        def dfs(root):
+            nonlocal res
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            res = max(res, left + right)
+            return 1 + max(left, right)
+				
+        dfs(root)
+        return res
+    
+def test_diameterOfBinaryTree():
+    root = TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3))
+
+    assert Solution().diameterOfBinaryTree(root) == 3
+
+    print("All tests passed")
+
+if __name__ == "__main__":
+    test_diameterOfBinaryTree()
